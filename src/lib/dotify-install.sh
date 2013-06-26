9d65d7831e4db0ac3587957654a5990af8a92909
Jim Myhrberg <contact@jimeh.me>
2013-06-27 00:05:19 +0200

    Rename run command to install

--- src/lib/dotify-install.sh
+++ src/lib/dotify-install.sh
@@ -1,8 +1,8 @@
-dotify-run() {
-  local dotfile="$(locate-dotfile "$@")"
+dotify-install() {
+  local dotfile="$(locate-dotfile)"
   if [ -z "$dotfile" ]; then return 1; fi
 
-  local target="$(locate-target "$@")"
+  local target="$(locate-target)"
   if [ -z "$target" ]; then return 1; fi
 
   parse-dotfile "$dotfile" "$target"
