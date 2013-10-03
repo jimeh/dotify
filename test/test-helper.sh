02ad286591d89039318a1225c79d15983c4b1831
Jim Myhrberg <contact@jimeh.me>
2013-10-03 23:35:25 +0100

    Break stub/restore test-helpers into their own stub.sh file

--- test/test-helper.sh
+++ test/test-helper.sh
@@ -3,6 +3,7 @@ testroot="$(dirname "$BASH_SOURCE")"
 
 # Include assert.sh testing library.
 source "$testroot/assert.sh"
+source "$testroot/stub.sh"
 
 #
 # Additional test helpers.
@@ -11,31 +12,4 @@ source "$testroot/assert.sh"
 # Silent shortcut to "cd -".
 cd-back() {
   cd - 1>/dev/null
-}
-
-# Stub commands printing it's name and arguments to STDOUT or STDERR.
-stub() {
-  local cmd="$1"
-  if [ "$2" == "STDERR" ]; then local redirect=" 1>&2"; fi
-
-  if [[ "$(type "$cmd" | head -1)" == *"is a function" ]]; then
-    local source="$(type "$cmd" | tail -n +2)"
-    source="${source/$cmd/original_${cmd}}"
-    eval "$source"
-  fi
-  eval "$(echo -e "${1}() {\n  echo \"$1 stub: \$@\"$redirect\n}")"
-}
-
-# Restore the original command/function that was stubbed with stub.
-restore() {
-  local cmd="$1"
-  unset -f "$cmd"
-  if type "original_${cmd}" &>/dev/null; then
-    if [[ "$(type "original_${cmd}" | head -1)" == *"is a function" ]]; then
-      local source="$(type "original_$cmd" | tail -n +2)"
-      source="${source/original_${cmd}/$cmd}"
-      eval "$source"
-      unset -f "original_${cmd}"
-    fi
-  fi
 }
