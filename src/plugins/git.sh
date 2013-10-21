83257088498dc882d25a055d8ef7725bab838563
Jim Myhrberg <contact@jimeh.me>
2013-10-21 01:31:58 +0100

    Flesh out basic structure of action plugins

--- src/plugins/git.sh
+++ src/plugins/git.sh
@@ -1,3 +1,15 @@
 dotify-action-git() {
-  echo "git: $@"
+  if [ "$DOTIFY_RUN_MODE" == "install" ]; then
+    dotify-action-git-install $@
+  elif [ "$DOTIFY_RUN_MODE" == "uninstall" ]; then
+    dotify-action-git-uninstall $@
+  fi
+}
+
+dotify-action-git-install() {
+  echo "git install: $@"
+}
+
+dotify-action-git-uninstall() {
+  echo "git uninstall: $@"
 }
--- src/plugins/link.sh
+++ src/plugins/link.sh
@@ -1,3 +1,15 @@
 dotify-action-link() {
-  echo "link: $@"
+  if [ "$DOTIFY_RUN_MODE" == "install" ]; then
+    dotify-action-link-install $@
+  elif [ "$DOTIFY_RUN_MODE" == "uninstall" ]; then
+    dotify-action-link-uninstall $@
+  fi
+}
+
+dotify-action-link-install() {
+  echo "link install: $@"
+}
+
+dotify-action-link-uninstall() {
+  echo "link uninstall: $@"
 }
